db74a7ab56b734644b126a004fe5b51fa66665c1
Harshan-mv <hv754310@gmail.com>
2026-08-09 10:46:46 +0530

    chnage config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // Point this at your running backend. Change to your deployed API URL in production.
 window.APP_CONFIG = {
-  API_BASE: 'https://mazha-nooki.onrender.com'
+  API_BASE: 'https://mazha-nooki.onrender.com/api'
 };
 
 
